8ada2a2e37146f46cea8fa0968531bf0ec69f92f
Nghiem Huu Hoai <Huuhoai0511@gmail.com>
2022-05-10 09:26:42 +0700

    update

---
+++ src/Chuong5/Tinh_Tong.java
@@ -0,0 +1,13 @@
+package Chuong5;
+
+public class Tinh_Tong {
+
+	public static void main(String[] args) {
+		int tong = 0;
+		for (int i = 0; i <= 8888; i+=2) {
+			tong = i;
+		}
+		System.out.println("Tong: " + tong);
+	}
+
+}
---
+++ src/Chuong5/Try_Catch.java
@@ -0,0 +1,18 @@
+package Chuong5;
+
+import java.util.Iterator;
+
+/*
+    try {
+      Các câu lệnh cần kiểm tra;
+      Nếu có lỗi thì catch sẽ được thực thi;
+    } catch (Exception e){
+      Nếu không có lỗi của try thì catch sẽ không thực thi;
+    }
+ */
+public class Try_Catch {
+
+	public static void main(String[] args) {
+		
+	}
+}
